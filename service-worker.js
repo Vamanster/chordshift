be122844f1e7f6fab293d183ee7abf01d2f1402c
Vamanster <vamanster@yahoo.com>
2025-02-27 11:27:09 +1300

    Service Worker test
    Testing the service worker with some basic files to cache

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const cacheName = 'chordshift-pwa-cache-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/app.js',
+  '/styles.css',
+  '/images/icon-192x192.png',
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('Service Worker: Caching files');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve from cache during fetch requests
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cacheResponse) => {
+      return cacheResponse || fetch(e.request);
+    })
+  );
+});
+
+// Clear old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((thisCacheName) => {
+          if (thisCacheName !== cacheName) {
+            console.log('Service Worker: Removing old cache', thisCacheName);
+            return caches.delete(thisCacheName);
+          }
+        })
+      );
+    })
+  );
+});
